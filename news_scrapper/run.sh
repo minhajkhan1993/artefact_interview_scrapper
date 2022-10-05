2f2c89e06ca041cff6e8735ca0a08fcd0541eab6
Khan <minhaj.khan@afiniti.com>
2022-10-05 05:49:04 +0500

    add mongodb env variables

--- news_scrapper/run.sh
+++ news_scrapper/run.sh
@@ -1,2 +1,7 @@
+export WEBSITE='https://www.bbc.com/news'
+export WEBSITE_BASE_URL='https://www.bbc.com'
+export MONGODB_URL="mongodb+srv://artefactassignement:mongoPass123@cluster0.nlv93t1.mongodb.net/?retryWrites=true&w=majority"
+export MONGO_DOCUMENT="bbc2"
+export MONGO_COLLECTION='news'
 scrapy crawl news
 uvicorn api.main:app --reload
