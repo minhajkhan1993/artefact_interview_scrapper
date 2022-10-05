91ee7a03f0fc9fc5922e952af6df7ccf7ae8c61b
Khan <minhaj.khan@afiniti.com>
2022-10-06 04:11:42 +0500

    refactor page insert functionality

--- news_scrapper/mongodb/insert.py
+++ news_scrapper/mongodb/insert.py
@@ -1,10 +1,18 @@
 from mongodb.mongodb import MongoDB
 
 
-def insert_article(mongodb: MongoDB, database: str, collection: str, article: dict):
-    mongodb.set_db(database)
-    mongodb.set_collection(collection)
+# inserts the provided article object into the mongodb collection specified by the args. 
+def insert_article(connection_url: str, database: str, collection: str, article: dict):
+    
+    # create mongodb client
+    mongo_instance = MongoDB()
+    mongo_instance.create_mongo_client(connection_url)
+    
+    # set client to point to specified database and collection
+    mongo_instance.set_db(database)
+    mongo_instance.set_collection(collection)
 
-    result = mongodb.insert_document(article)
+    # submit article to mongodb's insert function
+    result = mongo_instance.insert_document(article)
 
     return result
--- news_scrapper/news_scrapper/spiders/page_scrappers.py
+++ news_scrapper/news_scrapper/spiders/page_scrappers.py
@@ -6,13 +6,24 @@ import os
 # module that contains the methods to process various types of pages on bbc website
 class PageScrapper():
     
+    # inserts page objects into mongodb database
     def insert_page(self,page_data):
-        mongo_instance = MongoDB()
-        mongo_instance.create_mongo_client(os.environ.get('MONGODB_URL'))
-
-        result = insertModule.insert_article(mongo_instance, os.environ.get('Mongo_Database'), os.environ.get('Mongo_Collection'), page_data)
+        
+        # submit page to be inserted to mongodb's insert module
+        result = insertModule.insert_article(os.environ.get('MONGODB_URL'), os.environ.get('Mongo_Database'), os.environ.get('Mongo_Collection'), page_data)
         return result
     
+
+    # build the object that will be stored in database
+    def make_article_object(self, url: str, headline: str, body: str) -> dict:
+        
+        data = {
+                "url": url, 
+                "headline": headline,
+                "body": body
+                }
+        
+        return data
     
     
     # parse the bbc news pages.
@@ -21,7 +32,10 @@ class PageScrapper():
     
         page_body_content = []
         
+        # search for divs within the article body that contain article text.
         for paragraph in enumerate(response.xpath('//div[@data-component="text-block"]')):
+            
+            # extract text from <p> or <b> tags
             bold_div_content = paragraph[1].xpath('div/p//b/text()').get()
             para_div_content = paragraph[1].xpath('div/p/text()').get()
 
@@ -31,11 +45,12 @@ class PageScrapper():
             if not para_div_content is None:
                 page_body_content.append(para_div_content)  
         
-        data = {
-                "url": url, 
-                "headline": response.css('[id="main-heading"]::text').get(),
-                "body": page_body_content  
-               }
+        
+        # extract headline from page
+        headline = response.css('[id="main-heading"]::text').get()
+        
+        # build the object that will be stored in database
+        data = self.make_article_object(url, headline, page_body_content)
         
         self.insert_page(data)
     
@@ -48,40 +63,47 @@ class PageScrapper():
 
         page_body_content = []
         
+        # search for tags within the article body that contain article text.
         article_body = response.css('[class="article"]')
         for paragraph in enumerate(article_body.xpath('//p')):
+            
+            # extract text from <p> tags found. 
             div_content = paragraph[1].xpath('text()').get()
+            
             if not div_content is None:
                 page_body_content.append(div_content)
 
-        data = {
-                "url": url, 
-                "headline": response.css('[class="article-headline__text b-reith-sans-font b-font-weight-300"]::text').get(),
-                "body": page_body_content
-                }
+
+        # extract headline from page
+        headline = response.css('[class="article-headline__text b-reith-sans-font b-font-weight-300"]::text').get()       
+        
+        # build the object that will be stored in database
+        data = self.make_article_object(url, headline, page_body_content)
         
         self.insert_page(data)
         
         yield {'url': url}
     
 
-    # parse the bbc news pages.
+    # parse the bbc audio/visual reports pages.
     def parse_av_page(self,response):
         url = response.request.url
     
         page_body_content = []
         
+         # search for tags within the article body that contain article text.
         for paragraph in enumerate((response.css('[class="ssrcss-1s1kjo7-RichTextContainer e5tfeyi1"]')).xpath('p')):
             para_div_content = paragraph[1].xpath('text()').get()
             
             if not para_div_content is None:
                 page_body_content.append(para_div_content)  
         
-        data = {
-                "url": url, 
-                "headline": response.css('[id="main-heading"]::text').get(),
-                "body": page_body_content  
-               }
+
+        # extract headline from page
+        headline = response.css('[id="main-heading"]::text').get()
+
+        # build the object that will be stored in database
+        data = self.make_article_object(url, headline, page_body_content)
         
         self.insert_page(data)
     
